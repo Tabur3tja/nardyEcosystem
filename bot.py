4d1bc7f7134744bd4b9a4561433f3e5d4d3a1da9
Alex Glazoff <poduniglazov@gmail.com>
2024-01-12 11:47:27 +0000

    Import webbrowser library. Set empty message handler.

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 import telebot
-
+import webbrowser
 bot = telebot.TeleBot('6870812177:AAFx3pZv96ETLcFPO7oVL25HI7ct3mVGwkA')
+
+@bot.message_handler()
+def main(message):
+    if message.text.lower()== '/start':
+        bot.send_message(message.chat.id, "ІДІ нахуй, ")
+    elif message.text.lower()== '/constitution':
+        webbrowser.open('https://docs.google.com/document/d/1FAxjB5v1Fdb4C1x4vguf9BA6Witj4S2kW3mnLilIIcg/edit')
+bot.polling(none_stop=True)
 
 
